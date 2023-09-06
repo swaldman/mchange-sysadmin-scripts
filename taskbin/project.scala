f1e7ae2d09fbaa417282bbdde1f9fd256f63d0f1
Steve Waldman <swaldman@mchange.com>
2023-09-06 13:34:31 -0400

    Use mchange-sysadmin-scala v0.0.6 final.

--- taskbin/project.scala
+++ taskbin/project.scala
@@ -1,5 +1,5 @@
 //> using scala "3.3.0"
-//> using dep "com.mchange::mchange-sysadmin-scala:0.0.5"
+//> using dep "com.mchange::mchange-sysadmin-scala:0.0.6"
 //> using dep "com.lihaoyi::os-lib:0.9.1"
 
 
