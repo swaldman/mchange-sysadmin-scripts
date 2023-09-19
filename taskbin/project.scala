eaf03b068b093d6609c47f0864b09bd489c16db3
tickle5 root <sysadmin@mchange.com>
2023-09-19 16:40:23 +0000

    Upgrade taskbin project to mchange-sysadmin-scala v0.0.10

--- taskbin/project.scala
+++ taskbin/project.scala
@@ -1,5 +1,5 @@
 //> using scala "3.3.0"
-//> using dep "com.mchange::mchange-sysadmin-scala:0.0.9"
+//> using dep "com.mchange::mchange-sysadmin-scala:0.0.10"
 //> using dep "com.lihaoyi::os-lib:0.9.1"
 
 
