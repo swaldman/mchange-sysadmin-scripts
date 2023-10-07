f817ff4f2583d72fda4b07ca0a142aab17fbff4a
Steve Waldman <swaldman@mchange.com>
2023-10-07 14:45:08 -0400

    Update mchange-sysadmin-scala version for 0.1.0 final.

--- project.scala
+++ project.scala
@@ -1,5 +1,5 @@
 //> using scala "3.3.1"
 //> using jvm 17
-//> using dep "com.mchange::mchange-sysadmin-scala:0.1.0-SNAPSHOT"
+//> using dep "com.mchange::mchange-sysadmin-scala:0.1.0"
 //> using dep "com.lihaoyi::os-lib:0.9.1"
 //> using option -deprecation
