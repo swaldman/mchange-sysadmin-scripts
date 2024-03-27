822c7da604213a016674c4ba8fd0d5edacdec6a9
Steve Waldman (as mchange-syadmin) <swaldman@mchange.com>
2024-03-27 03:34:11 +0000

    Update dependencies.

--- project.scala
+++ project.scala
@@ -1,5 +1,9 @@
 //> using scala "3.3.1"
 //> using jvm 17
-//> using dep "com.mchange::mchange-sysadmin-scala:0.1.2"
-//> using dep "com.lihaoyi::os-lib:0.9.1"
+//> using dep "com.mchange::mchange-sysadmin-scala:0.1.4"
+//> using dep "com.interfluidity::office-hours-interfluidity:0.0.2"
+//> using dep "com.lihaoyi::os-lib:0.9.3"
 //> using option -deprecation
+//> using repository https://www.mchange.com/repository
+
+
