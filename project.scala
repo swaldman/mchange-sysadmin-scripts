981509377e3c5149717aa7cefc185d797b5ef5ce
Steve Waldman <swaldman@mchange.com>
2025-11-04 16:14:02 +0000

    Upgrade os-lib to 0.11.6.

--- project.scala
+++ project.scala
@@ -2,7 +2,7 @@
 //> using jvm 21
 //> using dep "com.mchange::mchange-sysadmin-scala:0.1.4"
 //> using dep "com.interfluidity::office-hours-interfluidity:0.0.2"
-//> using dep "com.lihaoyi::os-lib:0.11.5"
+//> using dep "com.lihaoyi::os-lib:0.11.6"
 //> using option -deprecation
 //> using repository https://www.mchange.com/repository
 
