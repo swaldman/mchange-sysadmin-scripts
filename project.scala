c4cd62e0b5d54a689dcc02e19d7e6458cccac3cc
Steve Waldman <swaldman@mchange.com>
2025-11-03 21:41:36 -0500

    Update dependencies to recent versions.

--- project.scala
+++ project.scala
@@ -1,8 +1,8 @@
-//> using scala "3.3.1"
-//> using jvm 17
+//> using scala "3.3.7"
+//> using jvm 21
 //> using dep "com.mchange::mchange-sysadmin-scala:0.1.4"
 //> using dep "com.interfluidity::office-hours-interfluidity:0.0.2"
-//> using dep "com.lihaoyi::os-lib:0.9.3"
+//> using dep "com.lihaoyi::os-lib:0.11.5"
 //> using option -deprecation
 //> using repository https://www.mchange.com/repository
 
