346b6d30cbb2f88d42d5ed1a547084ddbf1bbf59
Steve Waldman <swaldman@mchange.com>
2025-11-03 21:55:55 -0500

    Fix bad slash in regex pattern...

--- taskbin/zipper.scala
+++ taskbin/zipper.scala
@@ -9,7 +9,7 @@ import math.Ordering.Implicits.infixOrderingOps
 object Zipper:
   case class Pad( lastZipTime : Option[Instant] = None, lastUpdateTime : Option[Instant] = None, tmpDir : Option[os.Path] = None )
   val TimestampFormatter = DateTimeFormatter.ofPattern("'-'yyyy'-'MM'-'dd'-'HH'h'mm'm'ss's'")
-  val TimestampedRegex = """^(.+)(\-\d{4}\-\d{2}\-\d{2}\-\d{2}h\d{2}\m\d{2}s)\.(.+)$""".r
+  val TimestampedRegex = """^(.+)(\-\d{4}\-\d{2}\-\d{2}\-\d{2}h\d{2}m\d{2}s)\.(.+)$""".r
 class Zipper( baseName : String, siteDir : os.Path, zipDir : os.Path ):
   import Zipper.*
 
